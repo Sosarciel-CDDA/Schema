6920d0052b31f18a25d3701791b4c3629f34644c
zwa73 <ssfen2419@126.com>
2025-06-22 20:36:17 +0800

    fx

--- src/Schema/WeaponCategory.ts
+++ src/Schema/WeaponCategory.ts
@@ -1,5 +1,21 @@
-import { CddaID } from "./GenericDefine";
+import { CddaID, DescText } from "./GenericDefine";
+import { ProficiencyID } from "./Proficiency";
 
+/**武器类型ID */
+export type WeaponCategoryID = CddaID<'WPCY'>|DefineWeaponCategoryID;
+
+/** 武器类别定义 - 用于定义武器分类 */
+export type WeaponCategory = {
+    type: "weapon_category";
+    /**武器类型唯一ID */
+    id: (WeaponCategoryID);
+    /**类别名称 */
+    name: (DescText);
+    /**使用此类武器时可能应用加成的熟练度列表
+     * 参见熟练度文档获取更多细节
+     */
+    proficiencies?: ProficiencyID[];
+};
 
 
 /**预定义的武器类型ID 列表 */
@@ -18,19 +34,17 @@ export const DefineWeaponCategoryIDList = [
     "SHIVS"                 , //匕首
     "HOOKING_WEAPONRY"      , //钩形武器
     "SPEARS"                , //矛
+    "UNARMED"               , //徒手
     "POLEARMS"              , //长柄武器
     "FENCING_WEAPONRY"      , //击剑武器
+    "LONG_THRUSTING_SWORDS" , //长刺剑
     "BIONIC_WEAPONRY"       , //仿生武器
     "BIONIC_SWORDS"         , //仿生剑
-    "MEDIEVAL_SWORDS"       , //中世纪剑
-    "CONSTRUCTED_SWORDS"    , //构造剑
-    "JAPANESE_SWORDS"       , //日本剑
     "GREAT_SWORDS"          , //大剑
     "GREAT_HAMMERS"         , //大锤
     "GREAT_AXES"            , //大斧
     "HAND_AXES"             , //手斧
+    "WHIPS"                 , //鞭子
 ] as const;
 /**预定义的武器类型 */
 export type DefineWeaponCategoryID = typeof DefineWeaponCategoryIDList[number];
-/**武器类型ID */
-export type WeaponCategoryID = CddaID<"WPCY"> | DefineWeaponCategoryID;
