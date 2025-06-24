9bd6b2e5e7bde67ec0d6fc491187183f6b8820f2
zwa73 <ssfen2419@126.com>
2025-06-24 15:18:13 +0800

    fx

--- scripts/compile.ps1
+++ scripts/compile.ps1
@@ -2,5 +2,5 @@
 Write-Output 开始删除原dist
 Remove-Item -Recurse -Force dist
 Write-Output 开始编译
-tsc -p tsconfig.json
-tsc-alias -p tsconfig.json
+tsc -p tsconfig.compile.json
+tsc-alias -p tsconfig.compile.json
