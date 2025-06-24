706530efcc624e009338e7f954801920fbb67d6c
zwa73 <ssfen2419@126.com>
2025-06-24 16:58:15 +0800

    fx

--- src/Macro.macro.ts
+++ src/Macro.macro.ts
@@ -1,10 +1,11 @@
 import {UtilMacro} from '@zwa73/dev-utils';
 import path from 'pathe';
-import { EOC_DIR, exportTypeList, Item_DIR } from 'Macro';
+import { EOC_DIR, exportTypeList, extractDefineList, Item_DIR } from 'Macro';
 
 //自动导出
 UtilMacro.exportComment('src/**/*.ts');
 
+//#region typelist转导
 // 使用宏生成EocEffectList
 void exportTypeList({
     listName:'EocEffectList',
@@ -17,21 +18,31 @@ void exportTypeList({
     ],
     region:'Eoc效果表导出'
 });
+
 void exportTypeList({
     listName:'BoolOperateList',
     targetFile:path.join(EOC_DIR,"VariableObject","BoolObjectIndex.ts"),
     typeFileList:[path.join(EOC_DIR,"VariableObject","BoolObject.ts")],
     region:'BoolOperate导出'
 });
+
 void exportTypeList({
     listName:'StrOperateList',
     targetFile:path.join(EOC_DIR,"VariableObject","StringObjectIndex.ts"),
     typeFileList:[path.join(EOC_DIR,"VariableObject","StringObject.ts")],
     region:'StrOperate导出'
 });
+
 void exportTypeList({
     listName:'UseActionList',
     targetFile:path.join(Item_DIR,"UseAction","UseActionIndex.ts"),
     typeFileList:[path.join(Item_DIR,"UseAction","UseAction.ts")],
     region:'UseAction导出'
 });
+//#endregion
+
+//#region 预定义IDList生成
+void extractDefineList({
+
+});
+//#endregion
--- src/Macro/Utils.ts
+++ src/Macro/Utils.ts
@@ -1,13 +1,21 @@
 import { regionMacro } from '@zwa73/dev-utils';
-import { MPromise } from '@zwa73/utils';
+import { memoize, MPromise, UtilFT } from '@zwa73/utils';
 import fs from 'fs';
 import path from 'pathe';
 
 // EocEffect目录路径
 export const SRC_DIR = path.join(__dirname,'..');
+export const DATA_PATH = path.join(SRC_DIR,'..','data');
+export const INFO_PATH = path.join(DATA_PATH,'buildinfo.json');
+
 export const SCHEMA_DIR = path.join(SRC_DIR,'Schema');
 export const EOC_DIR = path.join(SCHEMA_DIR,'Eoc');
 export const Item_DIR = path.join(SCHEMA_DIR,'Item');
+
+
+const loadBuildInfo = memoize(async ()=>{
+    return await UtilFT.loadJSONFile(INFO_PATH) as {gamepath:string};
+});
 
 /**生成TypeList并导出 */
 export async function exportTypeList(arg:{
@@ -57,20 +65,23 @@ export type ${listName} = [
 `.trim(),{filePath:targetFile})
 }
 
-/**生成数组 */
-export async function buildDefineList(arg:{
+/**生成预定义的数组数据 */
+export async function extractDefineList(arg:{
     region:string;
     /**输出的类型名 */
     typeName:string;
     /**生成的list所在文件 */
     targetFile:string;
-    /**读取的来源文件 */
+    /**读取的来源文件
+     * 路劲将以gamepath为起点
+     */
     sourceFileList:string[];
     /**转换函数 */
     func:(content:string)=>MPromise<string[]>;
 }){
+    const info = await loadBuildInfo();
     const {typeName,targetFile,sourceFileList,region,func} = arg;
-    const contextList = await Promise.all(sourceFileList.map(fp=>fs.promises.readFile(fp,'utf-8')));
+    const contextList = await Promise.all(sourceFileList.map(fp=>fs.promises.readFile(path.join(info.gamepath,fp),'utf-8')));
     const exportStringList = (await Promise.all(contextList.map(context => func(context)))).flat();
     const exportList = exportStringList.map(str=>`"${str}"`).join(',\n    ');
     return regionMacro(region,() => `
