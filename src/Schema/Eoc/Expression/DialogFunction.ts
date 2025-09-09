5cca3efa4479aeb297ab18cfc942470dc303b7b8
zwa73 <ssfen2419@126.com>
2025-09-09 16:27:57 +0800

    fx

--- src/Schema/Eoc/Expression/DialogFunction.ts
+++ src/Schema/Eoc/Expression/DialogFunction.ts
@@ -1,10 +1,21 @@
-import { BodyPartID } from "Schema/BodyPart";
-import { DamageTypeID } from "Schema/DamageType";
+import { DefineBodyPartID } from "Schema/BodyPart";
+import { DefineDamageTypeID } from "Schema/DamageType";
+import { SchemaString } from "Schema/GenericDefine";
 
 type UN  = 'u'|'n';
 type UNG = 'u'|'n'|'global';
 type G   = 'global';
 type Talker = UN|UNG|G;
 const pt = (talker:Talker)=>talker=='global' ? '' : `${talker}_`;
+type FNP<T extends string> = `'${T}'`|SchemaString;
 
-const armor = (talker:UN,damageType:DamageTypeID,bodypart:BodyPartID)=>`${pt(talker)}armor('${damageType}', '${bodypart}')`;
+
+/**获取对应部位的护甲值  
+ * 只读  
+ * 返回角色特定身体部位的护甲值, 对应特定伤害类型  
+ * @example
+ * "condition": { "math": [ "u armour('bash', 'torso') >= 5"] }
+ */
+const fnArmor = (talker:UN,damageType:FNP<DefineDamageTypeID>,bodypart:FNP<DefineBodyPartID>)=>
+    `${pt(talker)}armor(${damageType}, ${bodypart})`;
+
