ccfecefdaf8cf0647d35551e94e149f375feda9b
zwa73 <ssfen2419@126.com>
2025-09-10 19:47:51 +0800

    fx

--- src/Macro/ModDefine.macro.ts
+++ src/Macro/ModDefine.macro.ts
@@ -6,17 +6,21 @@ import fs from 'fs';
 
 regionMacro("ModDefineID生成",async ()=>{
     const filelist = await UtilFT.fileSearchGlob(SCHEMA_DIR,'**/*.ts');
-    const idlist = await Promise.all(filelist.map(async fp=>{
-        const text = await fs.promises.readFile(fp,'utf-8');
-        const match = text.match(/export type (\S+) = CddaID<('|")(.+)('|")>/);
-        if(match==null) return;
-        const idtext = match[1];
-        const ids = match[3];
-        return `
-/**生成适用于此mod的 ${idtext} */
-gen${idtext}(id:string):${idtext}{
-    return \`\${this.MOD_PREFIX}_${ids}_\${id}\`;
-}`.trim();
+    const idlist = await Promise.all(filelist.map(async fp => {
+        const text = await fs.promises.readFile(fp, 'utf-8');
+        const matches = [...text.matchAll(/export type (\S+) = CddaID<('|")(.+?)('|")>/g)];
+        if (matches.length === 0) return;
+        return matches.map(match => {
+            const idtext = match[1];
+            const ids = match[3];
+            return `
+    /**生成适用于此mod的 ${idtext} */
+    gen${idtext}(id:string):${idtext}{
+        return \`\${this.MOD_PREFIX}_${ids}_\${id}\`;
+    }
+`.trim();
+        });
     }));
+
     return idlist.filter(v=>v!=undefined).join('\n');
 },{filePath:path.join(SRC_DIR,'Util','ModDefine.ts')});
--- src/Util/ModDefine.ts
+++ src/Util/ModDefine.ts
@@ -1,8 +1,7 @@
 import {
     AmmunitionTypeID, BoolExpr, Eoc, EocEffect, EocEffectList, SpellID,
-    EffectID, TalkTopicID, EocID ,FlagID,AmmoID, ArmorID, GunID,
+    EffectID, TalkTopicID, EocID ,FlagID,
     ItemGroupID,MonsterID,MutationID,NpcClassID,NpcInstanceID, EnchantmentID,
-    ItemID,
     WeaponCategoryID,
     WeakpointSetID,
     VitaminID,
@@ -58,12 +57,16 @@ import {
     ActivityTypeID,
     ToolID,
     MagazineID,
+    ItemID,
     GunModID,
+    GunID,
     ItemVariantID,
     ComestibleID,
     BionicItemID,
     BatteryID,
-    ArtifactID
+    ArtifactID,
+    ArmorID,
+    AmmoID,
 } from "@/src/Schema";
 
 /**ModID生成工具 */
@@ -75,311 +78,307 @@ export class ModDefine{
     }
     //#region ModDefineID生成
     /**生成适用于此mod的 WeaponCategoryID */
-    genWeaponCategoryID(id:string):WeaponCategoryID{
-        return `${this.MOD_PREFIX}_WPCY_${id}`;
-    }
+        genWeaponCategoryID(id:string):WeaponCategoryID{
+            return `${this.MOD_PREFIX}_WPCY_${id}`;
+        }
     /**生成适用于此mod的 WeakpointSetID */
-    genWeakpointSetID(id:string):WeakpointSetID{
-        return `${this.MOD_PREFIX}_WPS_${id}`;
-    }
+        genWeakpointSetID(id:string):WeakpointSetID{
+            return `${this.MOD_PREFIX}_WPS_${id}`;
+        }
     /**生成适用于此mod的 VitaminID */
-    genVitaminID(id:string):VitaminID{
-        return `${this.MOD_PREFIX}_VIT_${id}`;
-    }
+        genVitaminID(id:string):VitaminID{
+            return `${this.MOD_PREFIX}_VIT_${id}`;
+        }
     /**生成适用于此mod的 VehiclePartID */
-    genVehiclePartID(id:string):VehiclePartID{
-        return `${this.MOD_PREFIX}_VP_${id}`;
-    }
+        genVehiclePartID(id:string):VehiclePartID{
+            return `${this.MOD_PREFIX}_VP_${id}`;
+        }
     /**生成适用于此mod的 VehicleID */
-    genVehicleID(id:string):VehicleID{
-        return `${this.MOD_PREFIX}_VEH_${id}`;
-    }
+        genVehicleID(id:string):VehicleID{
+            return `${this.MOD_PREFIX}_VEH_${id}`;
+        }
     /**生成适用于此mod的 TrapID */
-    genTrapID(id:string):TrapID{
-        return `${this.MOD_PREFIX}_TRAP_${id}`;
-    }
+        genTrapID(id:string):TrapID{
+            return `${this.MOD_PREFIX}_TRAP_${id}`;
+        }
     /**生成适用于此mod的 ToolQualityID */
-    genToolQualityID(id:string):ToolQualityID{
-        return `${this.MOD_PREFIX}_TQ_${id}`;
-    }
+        genToolQualityID(id:string):ToolQualityID{
+            return `${this.MOD_PREFIX}_TQ_${id}`;
+        }
     /**生成适用于此mod的 TerrainID */
-    genTerrainID(id:string):TerrainID{
-        return `${this.MOD_PREFIX}_TER_${id}`;
-    }
+        genTerrainID(id:string):TerrainID{
+            return `${this.MOD_PREFIX}_TER_${id}`;
+        }
     /**生成适用于此mod的 TechniqueID */
-    genTechniqueID(id:string):TechniqueID{
-        return `${this.MOD_PREFIX}_TECH_${id}`;
-    }
+        genTechniqueID(id:string):TechniqueID{
+            return `${this.MOD_PREFIX}_TECH_${id}`;
+        }
     /**生成适用于此mod的 SpellID */
-    genSpellID(id:string):SpellID{
-        return `${this.MOD_PREFIX}_SPELL_${id}`;
-    }
+        genSpellID(id:string):SpellID{
+            return `${this.MOD_PREFIX}_SPELL_${id}`;
+        }
     /**生成适用于此mod的 SpeedDescriptionID */
-    genSpeedDescriptionID(id:string):SpeedDescriptionID{
-        return `${this.MOD_PREFIX}_SPDESC_${id}`;
-    }
+        genSpeedDescriptionID(id:string):SpeedDescriptionID{
+            return `${this.MOD_PREFIX}_SPDESC_${id}`;
+        }
     /**生成适用于此mod的 SpeciesID */
-    genSpeciesID(id:string):SpeciesID{
-        return `${this.MOD_PREFIX}_SPEC_${id}`;
-    }
+        genSpeciesID(id:string):SpeciesID{
+            return `${this.MOD_PREFIX}_SPEC_${id}`;
+        }
     /**生成适用于此mod的 SoundEffectID */
-    genSoundEffectID(id:string):SoundEffectID{
-        return `${this.MOD_PREFIX}_SE_${id}`;
-    }
+        genSoundEffectID(id:string):SoundEffectID{
+            return `${this.MOD_PREFIX}_SE_${id}`;
+        }
     /**生成适用于此mod的 SnippetCategoryID */
-    genSnippetCategoryID(id:string):SnippetCategoryID{
-        return `${this.MOD_PREFIX}_SNIP_${id}`;
-    }
+        genSnippetCategoryID(id:string):SnippetCategoryID{
+            return `${this.MOD_PREFIX}_SNIP_${id}`;
+        }
     /**生成适用于此mod的 SkillID */
-    genSkillID(id:string):SkillID{
-        return `${this.MOD_PREFIX}_SKILL_${id}`;
-    }
+        genSkillID(id:string):SkillID{
+            return `${this.MOD_PREFIX}_SKILL_${id}`;
+        }
     /**生成适用于此mod的 ScentTypeID */
-    genScentTypeID(id:string):ScentTypeID{
-        return `${this.MOD_PREFIX}_SCENT_${id}`;
-    }
+        genScentTypeID(id:string):ScentTypeID{
+            return `${this.MOD_PREFIX}_SCENT_${id}`;
+        }
     /**生成适用于此mod的 RequirementID */
-    genRequirementID(id:string):RequirementID{
-        return `${this.MOD_PREFIX}_REQ_${id}`;
-    }
+        genRequirementID(id:string):RequirementID{
+            return `${this.MOD_PREFIX}_REQ_${id}`;
+        }
     /**生成适用于此mod的 RecipeID */
-    genRecipeID(id:string):RecipeID{
-        return `${this.MOD_PREFIX}_RECIPE_${id}`;
-    }
+        genRecipeID(id:string):RecipeID{
+            return `${this.MOD_PREFIX}_RECIPE_${id}`;
+        }
     /**生成适用于此mod的 ProficiencyCategoryID */
-    genProficiencyCategoryID(id:string):ProficiencyCategoryID{
-        return `${this.MOD_PREFIX}_PROFC_${id}`;
-    }
+        genProficiencyCategoryID(id:string):ProficiencyCategoryID{
+            return `${this.MOD_PREFIX}_PROFC_${id}`;
+        }
     /**生成适用于此mod的 ProficiencyID */
-    genProficiencyID(id:string):ProficiencyID{
-        return `${this.MOD_PREFIX}_PROF_${id}`;
-    }
+        genProficiencyID(id:string):ProficiencyID{
+            return `${this.MOD_PREFIX}_PROF_${id}`;
+        }
     /**生成适用于此mod的 ProfessionID */
-    genProfessionID(id:string):ProfessionID{
-        return `${this.MOD_PREFIX}_PROFESSION_${id}`;
-    }
+        genProfessionID(id:string):ProfessionID{
+            return `${this.MOD_PREFIX}_PROFESSION_${id}`;
+        }
     /**生成适用于此mod的 PaletteID */
-    genPaletteID(id:string):PaletteID{
-        return `${this.MOD_PREFIX}_PALETTE_${id}`;
-    }
+        genPaletteID(id:string):PaletteID{
+            return `${this.MOD_PREFIX}_PALETTE_${id}`;
+        }
     /**生成适用于此mod的 OvermapTerrainID */
-    genOvermapTerrainID(id:string):OvermapTerrainID{
-        return `${this.MOD_PREFIX}_OMTERR_${id}`;
-    }
+        genOvermapTerrainID(id:string):OvermapTerrainID{
+            return `${this.MOD_PREFIX}_OMTERR_${id}`;
+        }
     /**生成适用于此mod的 OverMapSpecialID */
-    genOverMapSpecialID(id:string):OverMapSpecialID{
-        return `${this.MOD_PREFIX}_OMSPEC_${id}`;
-    }
+        genOverMapSpecialID(id:string):OverMapSpecialID{
+            return `${this.MOD_PREFIX}_OMSPEC_${id}`;
+        }
     /**生成适用于此mod的 OptionSliderID */
-    genOptionSliderID(id:string):OptionSliderID{
-        return `${this.MOD_PREFIX}_OPTS_${id}`;
-    }
+        genOptionSliderID(id:string):OptionSliderID{
+            return `${this.MOD_PREFIX}_OPTS_${id}`;
+        }
     /**生成适用于此mod的 NpcInstanceID */
-    genNpcInstanceID(id:string):NpcInstanceID{
-        return `${this.MOD_PREFIX}_NPC_${id}`;
-    }
+        genNpcInstanceID(id:string):NpcInstanceID{
+            return `${this.MOD_PREFIX}_NPC_${id}`;
+        }
     /**生成适用于此mod的 NPCFactionID */
-    genNPCFactionID(id:string):NPCFactionID{
-        return `${this.MOD_PREFIX}_NPCF_${id}`;
-    }
+        genNPCFactionID(id:string):NPCFactionID{
+            return `${this.MOD_PREFIX}_NPCF_${id}`;
+        }
     /**生成适用于此mod的 NpcClassID */
-    genNpcClassID(id:string):NpcClassID{
-        return `${this.MOD_PREFIX}_NPCCLS_${id}`;
-    }
+        genNpcClassID(id:string):NpcClassID{
+            return `${this.MOD_PREFIX}_NPCCLS_${id}`;
+        }
     /**生成适用于此mod的 MutationCategoryID */
-    genMutationCategoryID(id:string):MutationCategoryID{
-        return `${this.MOD_PREFIX}_MUTC_${id}`;
-    }
+        genMutationCategoryID(id:string):MutationCategoryID{
+            return `${this.MOD_PREFIX}_MUTC_${id}`;
+        }
     /**生成适用于此mod的 MutationID */
-    genMutationID(id:string):MutationID{
-        return `${this.MOD_PREFIX}_MUT_${id}`;
-    }
+        genMutationID(id:string):MutationID{
+            return `${this.MOD_PREFIX}_MUT_${id}`;
+        }
     /**生成适用于此mod的 MoraleTypeID */
-    genMoraleTypeID(id:string):MoraleTypeID{
-        return `${this.MOD_PREFIX}_MORT_${id}`;
-    }
+        genMoraleTypeID(id:string):MoraleTypeID{
+            return `${this.MOD_PREFIX}_MORT_${id}`;
+        }
     /**生成适用于此mod的 MonsterGroupID */
-    genMonsterGroupID(id:string):MonsterGroupID{
-        return `${this.MOD_PREFIX}_MONG_${id}`;
-    }
+        genMonsterGroupID(id:string):MonsterGroupID{
+            return `${this.MOD_PREFIX}_MONG_${id}`;
+        }
     /**生成适用于此mod的 MonsterFactionID */
-    genMonsterFactionID(id:string):MonsterFactionID{
-        return `${this.MOD_PREFIX}_MONF_${id}`;
-    }
+        genMonsterFactionID(id:string):MonsterFactionID{
+            return `${this.MOD_PREFIX}_MONF_${id}`;
+        }
     /**生成适用于此mod的 MonsterID */
-    genMonsterID(id:string):MonsterID{
-        return `${this.MOD_PREFIX}_MON_${id}`;
-    }
+        genMonsterID(id:string):MonsterID{
+            return `${this.MOD_PREFIX}_MON_${id}`;
+        }
     /**生成适用于此mod的 MissionDefinitionID */
-    genMissionDefinitionID(id:string):MissionDefinitionID{
-        return `${this.MOD_PREFIX}_MISDEF_${id}`;
-    }
+        genMissionDefinitionID(id:string):MissionDefinitionID{
+            return `${this.MOD_PREFIX}_MISDEF_${id}`;
+        }
     /**生成适用于此mod的 MathFunctionID */
-    genMathFunctionID(id:string):MathFunctionID{
-        return `${this.MOD_PREFIX}_FUNC_${id}`;
-    }
+        genMathFunctionID(id:string):MathFunctionID{
+            return `${this.MOD_PREFIX}_FUNC_${id}`;
+        }
     /**生成适用于此mod的 MaterialID */
-    genMaterialID(id:string):MaterialID{
-        return `${this.MOD_PREFIX}_MATE_${id}`;
-    }
+        genMaterialID(id:string):MaterialID{
+            return `${this.MOD_PREFIX}_MATE_${id}`;
+        }
     /**生成适用于此mod的 MartialArtID */
-    genMartialArtID(id:string):MartialArtID{
-        return `${this.MOD_PREFIX}_MA_${id}`;
-    }
+        genMartialArtID(id:string):MartialArtID{
+            return `${this.MOD_PREFIX}_MA_${id}`;
+        }
     /**生成适用于此mod的 NestedMapgenID */
-    genNestedMapgenID(id:string):NestedMapgenID{
-        return `${this.MOD_PREFIX}_NESTMPG_${id}`;
-    }
+        genNestedMapgenID(id:string):NestedMapgenID{
+            return `${this.MOD_PREFIX}_NESTMPG_${id}`;
+        }
     /**生成适用于此mod的 LimbScoreID */
-    genLimbScoreID(id:string):LimbScoreID{
-        return `${this.MOD_PREFIX}_LS_${id}`;
-    }
+        genLimbScoreID(id:string):LimbScoreID{
+            return `${this.MOD_PREFIX}_LS_${id}`;
+        }
     /**生成适用于此mod的 ItemGroupID */
-    genItemGroupID(id:string):ItemGroupID{
-        return `${this.MOD_PREFIX}_ITEMGP_${id}`;
-    }
+        genItemGroupID(id:string):ItemGroupID{
+            return `${this.MOD_PREFIX}_ITEMGP_${id}`;
+        }
     /**生成适用于此mod的 ItemCategoryID */
-    genItemCategoryID(id:string):ItemCategoryID{
-        return `${this.MOD_PREFIX}_ITEMC_${id}`;
-    }
+        genItemCategoryID(id:string):ItemCategoryID{
+            return `${this.MOD_PREFIX}_ITEMC_${id}`;
+        }
     /**生成适用于此mod的 ItemActionID */
-    genItemActionID(id:string):ItemActionID{
-        return `${this.MOD_PREFIX}_IACT_${id}`;
-    }
+        genItemActionID(id:string):ItemActionID{
+            return `${this.MOD_PREFIX}_IACT_${id}`;
+        }
     /**生成适用于此mod的 HarvestDropTypeID */
-    genHarvestDropTypeID(id:string):HarvestDropTypeID{
-        return `${this.MOD_PREFIX}_HVDT_${id}`;
-    }
+        genHarvestDropTypeID(id:string):HarvestDropTypeID{
+            return `${this.MOD_PREFIX}_HVDT_${id}`;
+        }
     /**生成适用于此mod的 HarvestID */
-    genHarvestID(id:string):HarvestID{
-        return `${this.MOD_PREFIX}_HV_${id}`;
-    }
+        genHarvestID(id:string):HarvestID{
+            return `${this.MOD_PREFIX}_HV_${id}`;
+        }
     /**生成适用于此mod的 FurnitureID */
-    genFurnitureID(id:string):FurnitureID{
-        return `${this.MOD_PREFIX}_FURN_${id}`;
-    }
+        genFurnitureID(id:string):FurnitureID{
+            return `${this.MOD_PREFIX}_FURN_${id}`;
+        }
     /**生成适用于此mod的 CustomFlagID */
-    genCustomFlagID(id:string):CustomFlagID{
-        return `${this.MOD_PREFIX}_FLAG_${id}`;
-    }
+        genCustomFlagID(id:string):CustomFlagID{
+            return `${this.MOD_PREFIX}_FLAG_${id}`;
+        }
     /**生成适用于此mod的 FieldTypeID */
-    genFieldTypeID(id:string):FieldTypeID{
-        return `${this.MOD_PREFIX}_FD_${id}`;
-    }
+        genFieldTypeID(id:string):FieldTypeID{
+            return `${this.MOD_PREFIX}_FD_${id}`;
+        }
     /**生成适用于此mod的 FaultGroupID */
-    genFaultGroupID(id:string):FaultGroupID{
-        return `${this.MOD_PREFIX}_FAULTG_${id}`;
-    }
+        genFaultGroupID(id:string):FaultGroupID{
+            return `${this.MOD_PREFIX}_FAULTG_${id}`;
+        }
     /**生成适用于此mod的 FaultFixID */
-    genFaultFixID(id:string):FaultFixID{
-        return `${this.MOD_PREFIX}_FAULT_FIX_${id}`;
-    }
+        genFaultFixID(id:string):FaultFixID{
+            return `${this.MOD_PREFIX}_FAULT_FIX_${id}`;
+        }
     /**生成适用于此mod的 FaultID */
-    genFaultID(id:string):FaultID{
-        return `${this.MOD_PREFIX}_FAULT_${id}`;
-    }
+        genFaultID(id:string):FaultID{
+            return `${this.MOD_PREFIX}_FAULT_${id}`;
+        }
     /**生成适用于此mod的 EnchantmentID */
-    genEnchantmentID(id:string):EnchantmentID{
-        return `${this.MOD_PREFIX}_ENCH_${id}`;
-    }
+        genEnchantmentID(id:string):EnchantmentID{
+            return `${this.MOD_PREFIX}_ENCH_${id}`;
+        }
     /**生成适用于此mod的 EmitID */
-    genEmitID(id:string):EmitID{
-        return `${this.MOD_PREFIX}_EMIT_${id}`;
-    }
+        genEmitID(id:string):EmitID{
+            return `${this.MOD_PREFIX}_EMIT_${id}`;
+        }
     /**生成适用于此mod的 EffectID */
-    genEffectID(id:string):EffectID{
-        return `${this.MOD_PREFIX}_EFF_${id}`;
-    }
+        genEffectID(id:string):EffectID{
+            return `${this.MOD_PREFIX}_EFF_${id}`;
+        }
     /**生成适用于此mod的 CharacterModID */
-    genCharacterModID(id:string):CharacterModID{
-        return `${this.MOD_PREFIX}_CM_${id}`;
-    }
+        genCharacterModID(id:string):CharacterModID{
+            return `${this.MOD_PREFIX}_CM_${id}`;
+        }
     /**生成适用于此mod的 BodyPartID */
-    genBodyPartID(id:string):BodyPartID{
-        return `${this.MOD_PREFIX}_BP_${id}`;
-    }
+        genBodyPartID(id:string):BodyPartID{
+            return `${this.MOD_PREFIX}_BP_${id}`;
+        }
     /**生成适用于此mod的 BionicID */
-    genBionicID(id:string):BionicID{
-        return `${this.MOD_PREFIX}_BIO_${id}`;
-    }
+        genBionicID(id:string):BionicID{
+            return `${this.MOD_PREFIX}_BIO_${id}`;
+        }
     /**生成适用于此mod的 AttackVectorID */
-    genAttackVectorID(id:string):AttackVectorID{
-        return `${this.MOD_PREFIX}_ATKV_${id}`;
-    }
+        genAttackVectorID(id:string):AttackVectorID{
+            return `${this.MOD_PREFIX}_ATKV_${id}`;
+        }
     /**生成适用于此mod的 AmmoEffectID */
-    genAmmoEffectID(id:string):AmmoEffectID{
-        return `${this.MOD_PREFIX}_AEFF_${id}`;
-    }
+        genAmmoEffectID(id:string):AmmoEffectID{
+            return `${this.MOD_PREFIX}_AEFF_${id}`;
+        }
     /**生成适用于此mod的 AmmunitionTypeID */
-    genAmmunitionTypeID(id:string):AmmunitionTypeID{
-        return `${this.MOD_PREFIX}_AMMUNIT_${id}`;
-    }
+        genAmmunitionTypeID(id:string):AmmunitionTypeID{
+            return `${this.MOD_PREFIX}_AMMUNIT_${id}`;
+        }
     /**生成适用于此mod的 AddictionTypeID */
-    genAddictionTypeID(id:string):AddictionTypeID{
-        return `${this.MOD_PREFIX}_ADDICTION_TYPE_${id}`;
-    }
+        genAddictionTypeID(id:string):AddictionTypeID{
+            return `${this.MOD_PREFIX}_ADDICTION_TYPE_${id}`;
+        }
     /**生成适用于此mod的 ActivityTypeID */
-    genActivityTypeID(id:string):ActivityTypeID{
-        return `${this.MOD_PREFIX}_ACT_${id}`;
-    }
+        genActivityTypeID(id:string):ActivityTypeID{
+            return `${this.MOD_PREFIX}_ACT_${id}`;
+        }
     /**生成适用于此mod的 ToolID */
-    genToolID(id:string):ToolID{
-        return `${this.MOD_PREFIX}_TOOL_${id}`;
-    }
+        genToolID(id:string):ToolID{
+            return `${this.MOD_PREFIX}_TOOL_${id}`;
+        }
     /**生成适用于此mod的 MagazineID */
-    genMagazineID(id:string):MagazineID{
-        return `${this.MOD_PREFIX}_MAG_${id}`;
-    }
+        genMagazineID(id:string):MagazineID{
+            return `${this.MOD_PREFIX}_MAG_${id}`;
+        }
     /**生成适用于此mod的 ItemID */
-    genItemID(id:string):ItemID{
-        return `${this.MOD_PREFIX}_ITEM_${id}`;
-    }
+        genItemID(id:string):ItemID{
+            return `${this.MOD_PREFIX}_ITEM_${id}`;
+        }
     /**生成适用于此mod的 GunModID */
-    genGunModID(id:string):GunModID{
-        return `${this.MOD_PREFIX}_GMOD_${id}`;
-    }
+        genGunModID(id:string):GunModID{
+            return `${this.MOD_PREFIX}_GMOD_${id}`;
+        }
     /**生成适用于此mod的 GunID */
-    genGunID(id:string):GunID{
-        return `${this.MOD_PREFIX}_GUN_${id}`;
-    }
+        genGunID(id:string):GunID{
+            return `${this.MOD_PREFIX}_GUN_${id}`;
+        }
     /**生成适用于此mod的 ItemVariantID */
-    genItemVariantID(id:string):ItemVariantID{
-        return `${this.MOD_PREFIX}_ITEMV_${id}`;
-    }
+        genItemVariantID(id:string):ItemVariantID{
+            return `${this.MOD_PREFIX}_ITEMV_${id}`;
+        }
     /**生成适用于此mod的 ComestibleID */
-    genComestibleID(id:string):ComestibleID{
-        return `${this.MOD_PREFIX}_COME_${id}`;
-    }
+        genComestibleID(id:string):ComestibleID{
+            return `${this.MOD_PREFIX}_COME_${id}`;
+        }
     /**生成适用于此mod的 BionicItemID */
-    genBionicItemID(id:string):BionicItemID{
-        return `${this.MOD_PREFIX}_BIOITEM_${id}`;
-    }
+        genBionicItemID(id:string):BionicItemID{
+            return `${this.MOD_PREFIX}_BIOITEM_${id}`;
+        }
     /**生成适用于此mod的 BatteryID */
-    genBatteryID(id:string):BatteryID{
-        return `${this.MOD_PREFIX}_BATTERY_${id}`;
-    }
+        genBatteryID(id:string):BatteryID{
+            return `${this.MOD_PREFIX}_BATTERY_${id}`;
+        }
     /**生成适用于此mod的 ArtifactID */
-    genArtifactID(id:string):ArtifactID{
-        return `${this.MOD_PREFIX}_ARTIFACT_${id}`;
-    }
+        genArtifactID(id:string):ArtifactID{
+            return `${this.MOD_PREFIX}_ARTIFACT_${id}`;
+        }
     /**生成适用于此mod的 ArmorID */
-    genArmorID(id:string):ArmorID{
-        return `${this.MOD_PREFIX}_ARMOR_${id}`;
-    }
+        genArmorID(id:string):ArmorID{
+            return `${this.MOD_PREFIX}_ARMOR_${id}`;
+        }
     /**生成适用于此mod的 AmmoID */
-    genAmmoID(id:string):AmmoID{
-        return `${this.MOD_PREFIX}_AMMO_${id}`;
-    }
+        genAmmoID(id:string):AmmoID{
+            return `${this.MOD_PREFIX}_AMMO_${id}`;
+        }
     /**生成适用于此mod的 EocID */
-    genEocID(id:string):EocID{
-        return `${this.MOD_PREFIX}_EOC_${id}`;
-    }
+        genEocID(id:string):EocID{
+            return `${this.MOD_PREFIX}_EOC_${id}`;
+        }
     //#endregion
 
-    /**生成适用于此mod的 材质类型 ID */
-    genAmmuTypeID(id:string):AmmunitionTypeID{
-        return `${this.MOD_PREFIX}_AMMUNIT_${id}`;
-    }
     /**生成适用于此mod的 FLAG ID */
     genFlagID(id:string):FlagID{
         return this.genCustomFlagID(id);
