3f63bda46033d7c1a7ef87db4f1081db3cacc22b
zwa73 <ssfen2419@126.com>
2025-09-11 02:15:02 +0800

    fx

--- src/Schema/Eoc/EocEffect/EocEffect.ts
+++ src/Schema/Eoc/EocEffect/EocEffect.ts
@@ -2,19 +2,36 @@ import { ItemID } from "Schema/Item";
 import { TalkerVar } from "../Eoc";
 import { IDExpr, NumberExpr } from "../Expression";
 import { ItemGroupID } from "Schema/ItemGroup";
+import { FlagID } from "Schema/Flag";
 
 
 /**生成物品 */
 export type SpawnItem = TalkerVar<{
     spawn_item: IDExpr<ItemID>|IDExpr<ItemGroupID>;
-    /**数量 */
+    /**数量
+     * @default 1
+     */
     count?: (NumberExpr);
-    /**容器 */
+    /**容器
+     * @default null
+     */
     container?: IDExpr<ItemID>;
-    /**使用物品组 默认false*/
+    /**使用物品组
+     * @default false
+     */
     use_item_group?: boolean;
-    /**不显示消息 默认false*/
+    /**不显示消息
+     * @default false
+     */
     suppress_message?: boolean;
+    /**角色将尝试装备物品
+     * @default false
+     */
+    force_equip?: boolean;
+    /**物品将拥有这些flag
+     * @default []
+     */
+    flags: FlagID[];
 },"spawn_item">
 
 /**删除物品 */
