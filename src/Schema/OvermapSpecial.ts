98269717fb5b2b3b5cbc6fdf545dc4c9e1ed0517
zwa73 <ssfen2419@126.com>
2026-04-19 19:50:53 +0800

    fx

--- src/Schema/OvermapSpecial.ts
+++ src/Schema/OvermapSpecial.ts
@@ -1,5 +1,6 @@
+import { FlagID } from "./Flag";
 import { CddaID } from "./GenericDefine";
-import {  } from "./Mapgen";
+import { } from "./Mapgen";
 import { OvermapTerrainID } from "./OvermapTerrain";
 
 /**大地图特殊地点ID */
@@ -20,9 +21,22 @@ export type OverMapSpecial = {
     id: (OverMapSpecialID);
     overmaps: OverMapGen[];
     locations: ["field"];
+    /**与道路的连接点 */
     connections: GenPos[];
     city_distance: [number, number];
     city_sizes: [number, number];
+    /**刷新概率
+     * 无unique类flag下行为等同于 个数 = randInt(occurrences[1],occurrences[0])
+     * 有unique类flag时行为等同于 个数 = randFloat(0,1) < (occurrences[0] / occurrences[1])
+     */
     occurrences: [number, number];
-    flags: ["MILITARY", "UNIQUE", "MAN_MADE"];
+    flags: DefineOverMapSpecialFlagID[];
 };
+
+const DefineOverMapSpecialFlagIDList = [
+    "MILITARY"       ,
+    "GLOBAL_UNIQUE"  , // 全局唯一
+    "OVERMAP_UNIQUE" , // 单个大地图唯一
+    "MAN_MADE"       , // 标记在传统模式下会被移除的flag
+] as const;
+type DefineOverMapSpecialFlagID = typeof DefineOverMapSpecialFlagIDList[number] | FlagID;
